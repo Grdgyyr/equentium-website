497b7fdf67cf4ffa46a5950a2710ed7974551df0
ron <ronmagpantay1@gmail.com>
2025-04-07 14:03:08 +0800

    Add initial project structure with components, assets, and configuration files

---
+++ src/components/Banner.tsx
@@ -0,0 +1,12 @@
+export const Banner = () => {
+  return (
+    <div className="py-3 text-center bg-[linear-gradient(to_right, #FCD6FF, #29D8FF, #FFFD80, #F89ABF, #FCD6FF)]">
+      <div className="container">
+        <p className="font-medium">
+          <span className="hidden sm:inline">Introducigng asd asd as dasd -- </span>
+        <a href="#" className="underline underline-offset-4">Explore</a>
+        </p>
+      </div>
+    </div>
+  );
+};
