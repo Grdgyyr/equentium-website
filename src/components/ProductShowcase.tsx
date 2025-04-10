e2e3d2df4b59ad82fdf5c47ed073916c8d110d2b
plip <johnphilip.ravena@gmail.com>
2025-04-10 16:05:01 +0800

    Implement ProductShowcase component with scroll-based animations and responsive design

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,5 +1,45 @@
+"use client";
 import appScreen from "../assets/images/app-screen.png";
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const ProductShowcase = () => {
-  return null;
+  const appImage = useRef<HTMLImageElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: appImage,
+    offset: ["start end", "end end"],
+  });
+  const rotateX = useTransform(scrollYProgress, [0, 1], [15, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+
+  return (
+    <div className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CA8] py-[72px] py-24">
+      <div className="container">
+        <h2 className="text-center text-5xl sm:text6-xl font-bold tracking-tighter">Intuitive Interface</h2>
+        <div className="max-w-xl mx-auto">
+          <p className="text-xl text-center text-white/70 mt-5">
+            Celebrate the joy of accomplishment with an app designed to track your
+            progress, motivate your efforts and celebrate your success, one task at a time.
+          </p>
+        </div>
+        <div className="flex justify-center items-center mt-14"> {/* Centering container */}
+          <motion.div
+            style={{
+              opacity: opacity,
+              rotateX: rotateX,
+              transformPerspective: "800px",
+            }}
+          >
+            <Image
+              src={appScreen}
+              alt="The Product Screenshot"
+              className="block" // Ensure the image behaves as a block element
+              ref={appImage}
+            />
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
 };
